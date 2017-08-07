9e3c36c58f4ed810b7e7ad92aa6ee8cc5f52f67d
madeel385 <madeel385@gmail.com>
2017-08-07 17:29:20 +0500

    nxt chng

--- minchat.py
+++ minchat.py
@@ -1,4 +1,27 @@
+#!/usr/bin/env python
+
+from flask import Flask ,request
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "request method %s" % request.method
+
+
+@app.route("/adi",methods=['POST'])
+def adi():
+    return "request method "
+
+@app.route("/user/<username>")
+def user(username):
+    return '<h2>hello %s</h2>' % username
+
+@app.route("/post/<int:postid>")
+def post_id(postid):
+    return '<h2>your post id is  %s</h2>' % postid
 
 
 
-print ( "Hello World")
+if __name__ == "__main__":
+    app.run(host='127.0.0.1')
