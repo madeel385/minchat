e18407499a2cbd96f668e1987d636fecb3577f32
madeel385 <madeel385@gmail.com>
2017-08-08 12:31:10 +0500

    handle msg

--- minchat.py
+++ minchat.py
@@ -20,19 +20,7 @@ def HandleVerification():
     else:
         return "not allowed!!"
 
-<<<<<<< HEAD
-'''@app.route('/', methods=['POST'])
-=======
 
-@app.route('/', methods=['GET'])
->>>>>>> 4691cd428c32ca96e69b10033473aed8f35b2ade
-def handle_incoming_messages():
-    data = request.json
-    sender = data['entry'][0]['messaging'][0]['sender']['id']
-    message = data['entry'][0]['messaging'][0]['message']['text']
-    reply(sender, message[::-1])
- 
-    return "ok"'''
 
 @app.route("/", methods=['POST'])
 def handle_incoming_messages():
